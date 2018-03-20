94faf88ee4b48f69317690276668cf9618babaa7
Joanne Lee <vernapii@gmail.com>
2018-03-20 00:41:42 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -2,7 +2,8 @@ describe('Beatles Loops', function() {
   it("returns an array of strings containing what instruments each instrument plays", function() {
     const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
     const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-    
+       expect(theBeatlesPlay(musicians, instruments)).toEqual(["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);
+ const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];	 });
   describe('johnLennonFacts', function(){
     it("returns an array of strings with exclamation points", function() {
       expect(johnLennonFacts([
